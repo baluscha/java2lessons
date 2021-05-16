7226ffc5dca8cb988d97ad9c393ebb5f63d61d9c
Anna Balueva <baluscha@yandex.ru>
2021-05-17 00:09:19 +0300

    code from lesson1

---
+++ src/Lesson1/Pool.java
@@ -0,0 +1,17 @@
+package Lesson1;
+
+public class Pool {
+    private int length;
+
+    public Pool(int length) {
+        this.length = length;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+}
---
+++ src/Lesson3/Test2.java
@@ -0,0 +1,20 @@
+package Lesson3;
+
+public class Test2 {
+    public static void main(String[] args) {
+        int a = 100;
+        int b = 30;
+        double c = a % b;
+        System.out.println(c);
+
+        int i1 = 5;
+        int i2 = 11;
+        double d1 = 5.5;
+        double d2 = 1.3;
+        long l = 20l;
+        double result = 0;
+        result = i2 / d1 + d2 % i1 - l;
+        System.out.println(result);
+    }
+
+}
